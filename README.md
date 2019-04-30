8006a8e800f1bb595966196d442bcb411d8cd3a0
Anthony Fu <anthonyfu117@hotmail.com>
2019-04-30 17:33:51 +0800

    docs: add badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Operation Sync
+
+[![npm](https://img.shields.io/npm/v/operation-sync.svg)](https://www.npmjs.com/package/operation-sync)
--- src/operation-sync.ts
+++ src/operation-sync.ts
@@ -64,7 +64,7 @@ export default class OperationSync<S> {
 
   insertOperations(operations: OperationOption[]) {
     const processed = this.processOperations(operations)
-    this.operations = this.operations.concat(processed).sort((a,b) => a.timestamp - b.timestamp)
+    this.operations = this.operations.concat(processed).sort((a, b) => a.timestamp - b.timestamp)
   }
 
   treeHash(operationIndex: number) {
